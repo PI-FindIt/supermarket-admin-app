16c648bf262576c6449c36fc625d17bbbbc6ed3e
Brupez <brunolop50@gmail.com>
2025-05-08 21:12:09 +0100

    store layout card with details

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,9 @@
 import { PaymentsOverview } from "@/components/Charts/payments-overview";
 import { UsedDevices } from "@/components/Charts/used-devices";
+import StoreLayout from "@/components/ImageLayout/index";
 import { WeeksProfit } from "@/components/Charts/weeks-profit";
 import { createTimeFrameExtractor } from "@/utils/timeframe-extractor";
 import { Suspense } from "react";
-//import { ChatsCard } from "./_components/chats-card";
-//import { OverviewCardsGroup } from "./_components/overview-cards";
-//import { OverviewCardsSkeleton } from "./_components/overview-cards/skeleton";
-import { RegionLabels } from "./_components/region-labels";
 import  TopProducts  from "@/components/Tables/top-products";
 import { TopProductsSkeleton } from "@/components/Tables/top-products/skeleton";
 
@@ -22,12 +19,6 @@ export default async function Home({ searchParams }: PropsType) {
 
   return (
     <>
-      {/* Values overview*/}
-
-      {/*<Suspense fallback={<OverviewCardsSkeleton />}>*/}
-      {/*  <OverviewCardsGroup />*/}
-      {/*</Suspense>*/}
-
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
         <PaymentsOverview
           className="col-span-12 xl:col-span-7"
@@ -53,13 +44,9 @@ export default async function Home({ searchParams }: PropsType) {
           timeFrame={extractTimeFrame("used_devices")?.split(":")[1]}
         />
 
-        <RegionLabels />
-
-        {/*Chat Suspense card*/}
-
-        {/*<Suspense fallback={null}>*/}
-        {/*  <ChatsCard />*/}
-        {/*</Suspense>*/}
+        <StoreLayout
+          className="col-span-12 xl:col-span-7"
+        />
       </div>
     </>
   );
