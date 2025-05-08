3bfcd237efcf324119f8d4047862ce70d31e9b3f
Brupez <brunolop50@gmail.com>
2025-05-08 19:54:35 +0100

    fetch data with IP address dinamically

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,32 +3,43 @@
 import { SidebarProvider } from "@/components/Layouts/sidebar/sidebar-context";
 import { ThemeProvider } from "next-themes";
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-
-const httpLink = createHttpLink({
-  uri: "http://192.168.40.43",
-  credentials: 'same-origin',
-  headers: {
-    "Content-Type": "application/json",
-  }
-
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'all',
-    },
-    query: {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'all',
-    }
-  },
-});
+import { useState, useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [client, setClient] = useState<ApolloClient<any> | null>(null);
+
+  useEffect(() => {
+    const url = `http://${window.location.hostname}`;
+
+    const httpLink = createHttpLink({
+      uri: url,
+      credentials: 'same-origin',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+
+    const apolloClient = new ApolloClient({
+      link: httpLink,
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        watchQuery: {
+          fetchPolicy: 'network-only',
+          errorPolicy: 'all',
+        },
+        query: {
+          fetchPolicy: 'network-only',
+          errorPolicy: 'all',
+        }
+      },
+    });
+
+    setClient(apolloClient);
+  }, []);
+
+  if (!client) return null;
+
+
   return (
     <ApolloProvider client={client}>
       <ThemeProvider defaultTheme="light" attribute="class">
